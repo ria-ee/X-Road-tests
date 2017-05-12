f77eb3b0a2c9006f6100bf81059452f5546a8107
sluhtoja <sluhtoja@bitbucket.org>
2017-05-12 11:12:26 +0300

    push

--- common/xrd-automated-tests/tests/xroad_client_registration_in_ss_221/client_registration_in_ss_2_2_1.py
+++ common/xrd-automated-tests/tests/xroad_client_registration_in_ss_221/client_registration_in_ss_2_2_1.py
@@ -391,7 +391,7 @@ def add_client_to_ss(self, client, retry_interval=0, retry_timeout=0, wait_input
             self.log(step + 'Client row not found')
 
     # Click "OK"
-    time.sleep(5)
+    time.sleep(10)
     self.wait_until_visible(type=By.XPATH, element=clients_table_vm.SELECT_CLIENT_POPUP_OK_BTN_XPATH).click()
     self.wait_jquery()
 
@@ -402,7 +402,7 @@ def add_client_to_ss(self, client, retry_interval=0, retry_timeout=0, wait_input
     self.wait_jquery()
     time.sleep(5)
 
-    self.input(subsystem_input, client['subsystem_code'])
+    self.input(subsystem_input, client['subsystem_code'], click=False)
 
     # Try to add client
     time.sleep(wait_input)
